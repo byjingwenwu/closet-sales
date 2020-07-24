55254e5e9b6662f19c2f52b92ca0b0a550bb8b2a
Jingwen Wu <wuesperanza@gmail.com>
2020-07-24 01:52:04 -0700

    modal and style

---
+++ client/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="d-flex justify-content-center align-items-center footer">
+        <h6 className="theme-color"> ©All rights reserved</h6>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
